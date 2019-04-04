2f983589822a1000c71e1a4a77cd1a5b403d77e8
xuansonkrt <xuansonkrt@gmail.com>
2019-04-04 21:16:48 +0700

    sonnx

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-
+    $(".js-show-modal1").on("click",function () {
+        var productId = $(this).data("product");
+        console.log("product: ",productId);
+    });
 });
